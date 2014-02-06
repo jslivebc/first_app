1566cc27eedc44f3413c392f494e7a6d496bbfb2
JS Live <jslive86@gmail.com>
2014-02-06 21:20:08 +1100

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '4fe1c930f67ae68b256985edf03b95335e96afbd3b1597a82dc32c7cacef8fec4f7ea7ca252274f340778f4ba9f0e695df18fe1186faa8da21f6021ef405ea5f'
